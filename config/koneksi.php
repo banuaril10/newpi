db3338040f8c261569dc97105d71498ba57608f0
Banu Ari Ramadhan <banuaril100@gmail.com>
2022-11-07 08:44:00 +0700

    07 11 2022

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,17 +2,7 @@
 date_default_timezone_set('Asia/Jakarta');
 error_reporting(0);
 
-// $options = array(
-    // PDO::ATTR_ERRMODE    => PDO::ERRMODE_SILENT
-// );
 try {
-    // $dbuser = 'postgres';
-    // $dbpass = 'idm$$&!@#141';
-    // $dbhost = '114.141.55.178';
-    // $dbname='postgres';
-    // $dbport='5432';
-    // $dbport='5434';
-
 	$dbuser = 'root';
     $dbpass = '';
     $dbhost = 'localhost';
@@ -23,8 +13,6 @@ try {
     $connec = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbport", $dbuser, $dbpass);
   
 } catch (PDOException $e) {
-    // echo "Error : " . $e->getMessage() . "<br/>";
-    // die();
 
 }
 
