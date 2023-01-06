e8dc9dde1f1dde84c2d513ad206f7e325c37904f
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-01-06 14:43:39 +0700

    05 01 2023

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,12 +3,23 @@ date_default_timezone_set('Asia/Jakarta');
 error_reporting(0);
 
 try {
+	// $dbuser = 'root';
+    // $dbpass = '';
+    // $dbhost = 'localhost';
+    // $dbname='poserp';
+    // $dbport='3306';
+	
 	$dbuser = 'adminpos';
     $dbpass = 'pwdposadmin';
     $dbhost = 'localhost';
-    // $dbhost = '10.0.44.2';
     $dbname='poserp';
     $dbport='3306';
+	
+	// $dbuser = 'adminpos';
+    // $dbpass = 'pwdposadmin';
+    // $dbhost = '10.0.44.2';
+    // $dbname='poserp';
+    // $dbport='3306';
 
 
     $connec = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbport", $dbuser, $dbpass);
