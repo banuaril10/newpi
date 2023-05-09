0a93d9cfd1a8101fe1d68b1de263cd82e485fd4a
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-05-09 13:19:41 +0700

    minor bug

--- api/update_version.php
+++ api/update_version.php
@@ -10,4 +10,4 @@ function execPrint($command) {
 }
 // Print the exec output inside of a pre element
 execPrint('D: && cd /xampp/htdocs/pi && git config --global --add safe.directory "*" && git config --global user.email "banuaril100@gmail.com" && git stash && git pull');
-execPrint('php update.php');
+// execPrint('php update.php');
