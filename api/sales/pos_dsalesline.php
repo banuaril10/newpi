e283db0c7daf89df74dab57a7362cac612cedcbe
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-06-07 16:24:14 +0700

    minor bug

---
+++ api/sales/pos_dsalesline.php
@@ -0,0 +1,410 @@
+<?php session_start();
+include "koneksi.php";
+ini_set('max_execution_time', '4000');
+$store_code = "";
+$org_id = "";
+$ad_muser_key = $_SESSION['ad_muser_key'];
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
+$getkode = $connec->query("select * from m_profile limit 1");
+foreach($getkode as $gk){
+	
+	$store_code = $gk['storecode'];
+	$org_id = $gk['storeid'];
+	$alamat = $gk['alamat'];
+	$alamat1 = $gk['alamat1'];
+	$kota = $gk['kota'];
+	$brand = $gk['brand'];
+	$footer1 = $gk['footer1'];
+	$footer2 = $gk['footer2'];
+	$footer3 = $gk['footer3'];
+	$setpoint = $gk['setpoint'];
+	$tipepoint = $gk['tipepoint'];
+	
+}
+
+$no_struk_cook = $store_code.'-'.date('YmdHis');
+
+
+
+
+function guid(){
+	return str_replace("-","",sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+		mt_rand( 0, 0xffff ),
+		mt_rand( 0, 0x0fff ) | 0x4000,
+		mt_rand( 0, 0x3fff ) | 0x8000,
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+	));
+}
+function rupiah($angka){
+	
+	$hasil_rupiah = number_format($angka,0,',','.');
+	return $hasil_rupiah;
+ 
+}
+
+function get_data_stock_all($a){
+						    
+	// $fields_string = http_build_query($postData);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://newpos.idolmartidolaku.com/pos-api/sync-product?org='.$a,
+	//CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_pos_fix_new&org='.$a,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	// CURLOPT_POSTFIELDS => $postData,
+	CURLOPT_HTTPHEADER => array(
+		'Authorization: Basic YmFudToxMjM0NTY='
+	)
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+function get_data_edc(){
+						    
+	// $fields_string = http_build_query($postData);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://newpos.idolmartidolaku.com/pos-api/sync-edc-bank',
+	//CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_pos_fix_new&org='.$a,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	// CURLOPT_POSTFIELDS => $postData,
+	CURLOPT_HTTPHEADER => array(
+		'Authorization: Basic YmFudToxMjM0NTY='
+	)
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+
+function get_data_member(){
+						    
+	// $fields_string = http_build_query($postData);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://newpos.idolmartidolaku.com/pos-api/sync-member',
+	//CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_pos_fix_new&org='.$a,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	// CURLOPT_POSTFIELDS => $postData,
+	CURLOPT_HTTPHEADER => array(
+		'Authorization: Basic YmFudToxMjM0NTY='
+	)
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+
+function get_data_promo($org_id){
+	
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_promo&org_id=".$org_id,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+	
+	
+}
+function get_data_promo_code($org_id){
+	
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_promo_code&org_id=".$org_id,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+	
+	
+}
+
+function get_data_promo_tebus($org_id){
+	
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://pi.idolmartidolaku.com/api/action.php?modul=inventory&act=sync_promo_tebus&org_id=".$org_id,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+	
+	
+}
+function get_data_cat(){
+						    
+	// $fields_string = http_build_query($postData);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://newpos.idolmartidolaku.com/pos-api/sync-category",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	// CURLOPT_POSTFIELDS => $postData,
+	CURLOPT_HTTPHEADER => array(
+		'Authorization: Basic YmFudToxMjM0NTY='
+	)
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+
+function pos_dcashierbalance($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dsalescashier.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+function pos_dsales($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dbilltoday.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+
+function pos_dsalesline($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dsalesline.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+function pos_dshopsales($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dsalesdaily.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
+
+function pos_dsalesdeleted($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dsalesdeleted.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+function toBase($num, $b=62) {
+  $base='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $r = $num  % $b ;
+  $res = $base[$r];
+  $q = floor($num/$b);
+  while ($q) {
+    $r = $q % $b;
+    $q =floor($q/$b);
+    $res = $base[$r].$res;
+  }
+  return $res;
+}
+
+
+			$items = array();
+
+			$query = $connec->query("select * from pos_dsales where date(insertdate) = date(now())");
+								foreach ($query as $r) {
+									$items[] = array(
+										'pos_dsalesline_key'	=>$r['pos_dsalesline_key'], 
+										'ad_mclient_key' 		=>$r['ad_mclient_key'], 
+										'ad_morg_key' 	=>$r['ad_morg_key'], 
+										'isactived' 	=>$r['isactived'], 
+										'insertdate' 	=>$r['insertdate'], 
+										'insertby' 		=>$r['insertby'], 
+										'postby' 		=>$r['postby'], 
+										'postdate' 		=>$r['postdate'], 
+										'pos_dsales_key' 	=>$r['pos_dsales_key'], 
+										'billno' 	=>$r['billno'], 
+										'seqno' 	=>$r['seqno'], 
+										'sku' 	=>$r['sku'], 
+										'qty' 	=>$r['qty'], 
+										'price' 	=>$r['price'], 
+										'discount' 	=>$r['discount'], 
+										'amount' 	=>$r['amount'], 
+										'issync' 	=>$r['issync'], 
+										'discountname' 	=>$r['discountname'], 
+										'buy' 	=>'0', 
+									);
+								
+								}	
+								$items_json = json_encode($items);
+								$hasil = pos_dsalesline($items_json);
+								var_dump($hasil);
+								// var_dump($items_json);
+								$j_hasil = json_decode($hasil, true);
+
+
+?>
+
+
+
+		
