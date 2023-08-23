e6961dc4cb3edf6bbe1d4dfd79fa0b4b852725c2
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-08-23 08:57:22 +0700

    commit message

--- api/action.php
+++ api/action.php
@@ -3518,7 +3518,7 @@ locator_name) VALUES (
 				}
 		
 		if (!$jj) {
-			$data = array("result"=>0, "msg"=>"Belum ada cash in yg di approved");
+			$data = array("result"=>0, "msg"=>"Gagal kirim ke newpos");
 			
 		}else{
 				$allarray = array("cashin"=>$jj);
