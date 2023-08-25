4af0e2e077552990d471ee85e73b8f35c9b4e71c
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-08-25 14:20:38 +0700

    commit message

--- cekperubahanharga.php
+++ cekperubahanharga.php
@@ -173,7 +173,7 @@
 						?>
 							
 							<tr>
-								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $row['name']; ?>|<?php echo $row['price']; ?>|<?php echo $row['tgl_sekarang']; ?>|<?php echo $row['rack_name']; ?>|<?php echo $row['barcode']; ?>|<?php echo $harga_last; ?>|<?php echo $row['tag']; ?>"></td>
+								<td><input type="checkbox" id="checkbox" name="checkbox[]" value="<?php echo $row['sku']; ?>|<?php echo $row['name']; ?>|<?php echo $row1['price']; ?>|<?php echo $row['tgl_sekarang']; ?>|<?php echo $row['rack_name']; ?>|<?php echo $row['barcode']; ?>|<?php echo $harga_last; ?>|<?php echo $row['tag']; ?>"></td>
 								<td scope="row"><?php echo $no; ?></td>
 								<td><?php echo $row['sku']; ?></td>
 								<td><?php echo $row['name']; ?></td>
