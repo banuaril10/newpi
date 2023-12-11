55f9bf1369cc34525b205b584401cc03b58ee270
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-12-11 09:33:01 +0700

    10-12-2023

--- api/sales/action_mypos.php
+++ api/sales/action_mypos.php
@@ -84,6 +84,31 @@ function pos_dsales($a){
 					
 }
 
+function pos_dsalesheader($a){
+			
+	// $fields_string = http_build_query($a);
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => 'https://pi.idolmartidolaku.com/api/sales_order/pos_dbilltoday.php?id=OHdkaHkyODczeWQ3ZDM2NzI4MzJoZDk3MzI4OTc5eDcyOTdyNDkycjc5N3N1MHI',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_POSTFIELDS => $a,
+	));
+	
+	$response = curl_exec($curl);
+	
+	curl_close($curl);
+	return $response;
+					
+					
+}
+
 
 function pos_dsalesline($a){
 			
@@ -229,6 +254,171 @@ if($_GET['modul'] == 'sales_order'){
 								foreach($j_hasil as $r){
 									// echo $r['data'];
 									$up = $connec->query("update pos_dsales set status_sales = '1' where pos_dsales_key = '".$r['data']."'");
+									if($up){
+										
+										$jum_sales++;
+									}
+								}
+								
+								echo "Berhasil kirim ".$jum_sales." data";
+								
+	}else if($_GET['act'] == 'pos_dsales'){
+			$items = array();
+
+			$date_now = date('Y-m-d');
+			// $date_yd = date('Y-m-d',strtotime(date('Y-m-d') . "-2 days"));
+			
+			if($_GET['tgl1'] && !empty($_GET['tgl1']) &&  $_GET['tgl2'] && !empty($_GET['tgl2'])){
+				
+				$query = $connec->query("	
+				select 
+				upper(replace(uuid(),'-','')) as pos_dbilltoday_key, 
+				'1' as isactived, 
+				insertdate, 
+				insertby, 
+				'Admin' as postby, 
+				postdate, 
+				ad_morg_key  as ad_org_id,
+				billno, 
+				billamount, 
+				paymentmethodname, 
+				membercard, 
+				cardno, 
+				approvecode, 
+				edcno, 
+				serialno, 
+				billstatus, 
+				paycashgiven, 
+				pos_mbank_key, 
+				pos_dcashierbalance_key, 
+				paygiven, 
+				printcount, 
+				bankname, 
+				pos_medc_key, 
+				billcode, 
+				point
+				from pos_dsales 
+				where date(insertdate) between '".$_GET['tgl1']."' and '".$_GET['tgl2']."'
+				group by 
+				insertdate, 
+				insertby, 
+				postdate, 
+				ad_morg_key  ,
+				billno, 
+				billamount, 
+				paymentmethodname, 
+				membercard, 
+				cardno, 
+				approvecode, 
+				edcno, 
+				serialno, 
+				billstatus, 
+				paycashgiven, 
+				pos_mbank_key, 
+				pos_dcashierbalance_key, 
+				paygiven, 
+				printcount, 
+				bankname, 
+				pos_medc_key, 
+				billcode ");
+			}else{
+				
+				// $query = $connec->query("select * from pos_dsales where date(insertdate) between '".$date_yd."' and '".$date_now."' and (status_sales = '0' or status_sales is null) ");
+				$query = $connec->query("
+				select 
+				upper(replace(uuid(),'-','')) as pos_dbilltoday_key, 
+				'1' as isactived, 
+				insertdate, 
+				insertby, 
+				'Admin' as postby, 
+				postdate, 
+				ad_morg_key  as ad_org_id,
+				billno, 
+				billamount, 
+				paymentmethodname, 
+				membercard, 
+				cardno, 
+				approvecode, 
+				edcno, 
+				serialno, 
+				billstatus, 
+				paycashgiven, 
+				pos_mbank_key, 
+				pos_dcashierbalance_key, 
+				paygiven, 
+				printcount, 
+				bankname, 
+				pos_medc_key, 
+				billcode, 
+				point
+				from pos_dsales 
+				where date(insertdate) between '".$date_now."' and '".$date_now."' and (status_sales_header = '0' or status_sales_header is null) 
+				group by 
+				insertdate, 
+				insertby, 
+				postdate, 
+				ad_morg_key  ,
+				billno, 
+				billamount,
+				paymentmethodname, 
+				membercard, 
+				cardno, 
+				approvecode, 
+				edcno, 
+				serialno, 
+				billstatus, 
+				paycashgiven, 
+				pos_mbank_key, 
+				pos_dcashierbalance_key, 
+				paygiven, 
+				printcount, 
+				bankname, 
+				pos_medc_key, 
+				billcode
+				");
+			}
+				
+				
+								foreach ($query as $r) {
+									$items[] = array(
+										'pos_dbilltoday_key'	=>$r['pos_dbilltoday_key'], 
+										'isactived' 		=>$r['isactived'], 
+										'insertdate' 	=>$r['insertdate'], 
+										'insertby' 	=>$r['insertby'], 
+										'postby' 	=>$r['postby'], 
+										'postdate' 	=>$r['postdate'], 
+										'ad_org_id' 	=>$r['ad_org_id'], 
+										'billno' 	=>$r['billno'], 
+										'billamount' 	=>$r['billamount'], 
+										'paymentmethodname' 	=>$r['paymentmethodname'], 
+										'membercard' 	=>$r['membercard'], 
+										'cardno' 	=>$r['cardno'], 
+										'approvecode' 	=>$r['approvecode'], 
+										'edcno' 	=>$r['edcno'], 
+										'serialno' 	=>$r['serialno'], 
+										'billstatus' 	=>$r['billstatus'], 
+										'paycashgiven' 	=>$r['paycashgiven'], 
+										'pos_mbank_key' 	=>$r['pos_mbank_key'], 
+										'pos_dcashierbalance_key' 	=>$r['pos_dcashierbalance_key'], 
+										'paygiven' 	=>$r['paygiven'], 
+										'printcount' 	=>$r['printcount'], 
+										'bankname' 	=>$r['bankname'], 
+										'pos_medc_key' 	=>$r['pos_medc_key'], 
+										'billcode' 	=>$r['billcode'], 
+										'point' 	=>$r['point']
+									);
+								
+								}	
+								$items_json = json_encode($items);
+								$hasil = pos_dsalesheader($items_json);
+								// var_dump($hasil);
+								// var_dump($items_json);
+								$j_hasil = json_decode($hasil, true);
+								// var_dump($hasil);
+								$jum_sales = 0;
+								foreach($j_hasil as $r){
+									// echo $r['data'];
+									$up = $connec->query("update pos_dsales set status_sales_header = '1' where billno = '".$r['data']."'");
 									if($up){
 										
 										$jum_sales++;
