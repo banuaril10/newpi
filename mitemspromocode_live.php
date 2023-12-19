924478963cd59e272f6f470c63fc0c4338c49f44
Banu Ari Ramadhan <banuaril100@gmail.com>
2023-12-19 10:23:34 +0700

    12-12-2023

--- mitemspromocode_live.php
+++ mitemspromocode_live.php
@@ -289,7 +289,8 @@
 </div>
 </div>
 
-<script src="styles/js/price-promo-tingkat.js"></script>
+<!--<script src="styles/js/price-promo-tingkat.js"></script>-->
+<script src="https://pi.idolmartidolaku.com/api/js/price-promo-tingkat.js"></script>
 <script type="text/javascript">
 $(document).ready( function () {
     $('#example').DataTable({
